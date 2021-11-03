eb95a0aa73efb5686e5e9ac1cfc72af9fea8c50b
jaxon mackey <termonaitor2004jaxon@gmail.com>
2021-11-03 15:39:56 -0500

    Update refresh.js

--- javascript/refresh.js
+++ javascript/refresh.js
@@ -3,4 +3,4 @@ function refresh() {
   document.getElementById("game-window").setAttribute("data", "http://www.new-ideaa.gq/pictures/object-main.png");
 }
 
-window.onload = changeData;
+(alert)window.onload = changeData;
